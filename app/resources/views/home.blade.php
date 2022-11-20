31410dc37a3f84f9d0b033748b7bb8ff85716840
wesllycode <wesllycode07@gmail.com>
2022-11-20 10:04:57 -0300

    feat: add router for home.blade.php and html page home.blade.php

---
+++ app/resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD livewire Laravel 9</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div clas="container-fluid">
+            <a class="navbar-brand" href="/">Livewire CRUD 9 </a>
+        </div>
+    </nav>
+
+
+
+
+</body>
+</html>
