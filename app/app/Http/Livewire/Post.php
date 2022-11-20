d5b7df305d3533ef1b0eab68ffd83543e071a90a
wesllycode <wesllycode07@gmail.com>
2022-11-20 09:49:53 -0300

    feat: puxar os dados dos posts

--- app/app/Http/Livewire/Post.php
+++ app/app/Http/Livewire/Post.php
@@ -3,11 +3,21 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Post as Posts;
 
 class Post extends Component
 {
+    public $posts;
+
+    // Validation Rules
+    protected $rules = [
+        'name' => 'required',
+        'description' => 'required'
+    ];
+
     public function render()
     {
+        $this->posts = Posts::select('id','name','description')->get();
         return view('livewire.post');
     }
 }
